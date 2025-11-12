eb50f6bb91e3501dd4047124732bcd74587dc233
Ethan7FJ <johanfetecua11@gmail.com>
2025-11-11 20:41:07 -0500

    Sql sentence modifique

--- src/controllers/inventary.controllers.ts
+++ src/controllers/inventary.controllers.ts
@@ -10,7 +10,7 @@ dotenv.config();
 /* GET PRODUCTS INVENTARY */
 export const ShowProducts = async (req: Request, res: Response) => {
     try {
-        const [productos] = await db.query('SELECT * FROM inventario');
+        const [productos] = await db.query('SELECT a.* , p.nombre_proveedor AS proveedor, c.nombre_categoria AS categoria, k.tipo_estado, z.nombre FROM inventario a INNER JOIN categoria c ON a.categoria_id = c.id INNER JOIN proveedor p ON a.proveedor_id = p.id INNER JOIN estado k ON a.estado_id = k.id INNER JOIN usuario z ON a.usuario_responsable = z.id');
         res.json({ success: true, productos: productos });
     } catch (err) {
         console.error(err);
